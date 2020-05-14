8710055aafda1f3637585b65b29b01bc65aa7946
Sahej Singh <sahejdeep@hotmail.com>
2020-05-14 19:22:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,4 +13,6 @@ return kittens}
 function destructivelyRemoveFirstKitten() {kittens.shift()
 return kittens}
 
-function appendKitten(name) {return [...kittens, name}
+function appendKitten(name){
+    return [...kittens, name]
+}
