ac58173bfb8b23bcc4a4a67f0720aa86a3063ad7
Sahej Singh <sahejdeep@hotmail.com>
2020-05-14 19:27:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,4 +23,4 @@ function prependKitten(name){
 
 function removeLastKitten() {return kittens.slice(0, kittens.length -1)}
 
-functin removeFirstKitten() {return kittens.slice(1)} 
+function removeFirstKitten() {return kittens.slice(1)} 
